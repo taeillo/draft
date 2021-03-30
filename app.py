7c31bb6862fbc7e21ec946123c56d85c02a50314
Admin <51963574+taeillo@users.noreply.github.com>
2021-03-30 11:00:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,14 @@
 from flask import Flask, request, redirect, jsonify, render_template 
 from googleapiclient.discovery import build
-from google.oauth2.credentials import Credentials
-
 from google.oauth2 import service_account
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 SERVICE_ACCOUNT_FILE = 'keys.json'
 
-creds = None
-creds = service_account.Credentials.from_service_account_file(
-        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-
-# If modifying these scopes, delete the file token.json.
-
-
-# The ID and range of a sample spreadsheet.
-SAMPLE_SPREADSHEET_ID = '1HQevvoML_FZVEHkwgD66Gyw836lgjR93v_zI632P070'
-
-
-
-
-service = build('sheets', 'v4', credentials=creds)
-
-# Call the Sheets API
-sheet = service.spreadsheets()
-result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID,
-                            range="Form Responses 1!A1").execute()
-values = result.get('values', [])
-
-print(values)
-
-
 username = None
 password = None
 
 app = Flask(__name__) 
-
 
 @app.route('/order', methods=['POST','GET'])
 def order():
@@ -61,6 +34,39 @@ def offline():
 @app.route('/asset-manifest.json')
 def manifest():
     return app.send_static_file('manifest.json')
+
+
+
+creds = service_account.Credentials.from_service_account_file(
+        SERVICE_ACCOUNT_FILE, scopes=SCOPES)
+
+service = build('sheets', 'v4', credentials=creds)
+
+# If modifying these scopes, delete the file token.json.
+
+
+# The ID and range of a sample spreadsheet.
+SAMPLE_SPREADSHEET_ID = '1HQevvoML_FZVEHkwgD66Gyw836lgjR93v_zI632P070'
+
+# Read from sheet
+
+sheet = service.spreadsheets()
+
+result = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID, range="Form Responses 1!A1:N20").execute()
+values = result.get('values', [])
+
+print(values)
+
+# Update to sheet
+
+data = [[]]
+
+updated_result = sheet.values().update(spreadsheetId=SAMPLE_SPREADSHEET_ID, range="Form Responses 1!A1", valueInputOption="USER_ENTERED", body={"values":data}).execute()
+
+print('{0} cells updated.'.format(updated_result.get('updatedCells')))
+
+
+
 
 
 if __name__ == "__main__":
